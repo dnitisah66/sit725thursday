277862b3373d3d70578f27961861cdd49e51615e
dnitisah66 <s225680282@deakin.edu.au>
2025-08-13 20:06:57 +1000

    week 5.1

---
+++ week5/public/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('#clickMeButton').click(() => {
+        alert("Thanks for clicking me. Hope you have a nice day!");
+    });
+    $('.collapsible').collapsible();
+    $('.modal').modal();
+});
